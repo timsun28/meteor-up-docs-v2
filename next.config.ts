9067dd3703a7ed7140d9a5953dcbbe0aa8de4903
Timo Frionnet <tfrionnet@gmail.com>
2025-09-10 18:41:37 +0200

    First commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import nextra from "nextra";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// Set up Nextra with its configuration
+const withNextra = nextra({
+    // ... Add Nextra-specific options here
+});
 
-export default nextConfig;
+// Export the final Next.js config with Nextra included
+export default withNextra({
+    // ... Add regular Next.js options here
+});
