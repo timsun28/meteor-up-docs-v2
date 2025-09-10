9cc9d62547e46bb1fb7a9a446ca73d0782373b41
Timo Frionnet <tfrionnet@gmail.com>
2025-09-10 18:42:06 +0200

    updated docs repo link

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
                     banner={banner}
                     navbar={navbar}
                     pageMap={await getPageMap()}
-                    docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+                    docsRepositoryBase="https://github.com/timsun28/meteor-up-docs-v2"
                     footer={footer}
                     // ... Your additional layout options
                 >
